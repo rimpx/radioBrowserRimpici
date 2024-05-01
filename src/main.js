e25ddacb4585c694b5cec493f3b05a983a6f5255
Riccardo Rimpici <133099369+rimpx@users.noreply.github.com>
2024-05-01 17:07:16 +0000

    .

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from 'vuetify' 
+import 'vuetify/dist/vuetify.min.css' 
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.use(vuetify) 
+
+app.mount('#app')
