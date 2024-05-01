a4528af68e6f9871b0a9cb4af2f080661317e226
Riccardo Rimpici <133099369+rimpx@users.noreply.github.com>
2024-05-01 17:20:38 +0000

    modifica con lista 1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,31 @@
 <template>
-  <div>
-    <v-container>
-      <v-row>
-        <v-col cols="12" sm="6" md="4" v-for="station in stations" :key="station.stationuuid">
-          <v-card>
-            <v-card-title>{{ station.name }}</v-card-title>
-            <v-card-subtitle>{{ station.tags }}</v-card-subtitle>
-            <v-card-actions>
-              <v-btn color="primary" @click="playRadio(station.url)">Play</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
+  <v-container>
+    <v-text-field
+      v-model="search"
+      append-icon="mdi-magnify"
+      label="Cerca stazioni"
+      single-line
+      hide-details
+    ></v-text-field>
+    <v-row>
+      <v-col
+        cols="12"
+        sm="6"
+        md="4"
+        v-for="station in filteredStations"
+        :key="station.stationuuid"
+      >
+        <v-card>
+          <v-card-title>{{ station.name }}</v-card-title>
+          <v-card-subtitle>{{ station.tags }}</v-card-subtitle>
+          <v-card-actions>
+            <v-btn color="primary" @click="playRadio(station.url)">Play</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
     <audio ref="audioPlayer" controls style="width: 100%;"></audio>
-  </div>
+  </v-container>
 </template>
 
 <script>
@@ -24,12 +35,20 @@ export default {
   data() {
     return {
       stations: [],
-      currentStation: null
+      search: ''
     };
+  },
+  computed: {
+    filteredStations() {
+      return this.stations.filter(station =>
+        station.name.toLowerCase().includes(this.search.toLowerCase())
+      );
+    }
   },
   methods: {
     fetchStations() {
-      axios.get('https://de1.api.radio-browser.info/json/stations')
+      axios
+        .get('https://de1.api.radio-browser.info/json/stations?limit=100')
         .then(response => {
           this.stations = response.data;
         })
@@ -41,7 +60,6 @@ export default {
       const audio = this.$refs.audioPlayer;
       audio.src = url;
       audio.play();
-      this.currentStation = url;
     }
   },
   mounted() {
@@ -51,5 +69,5 @@ export default {
 </script>
 
 <style scoped>
-/* Aggiungi qui lo stile CSS se necessario */
+/* Qui puoi aggiungere stili CSS specifici per questo componente */
 </style>
