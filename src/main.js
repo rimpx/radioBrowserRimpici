6bf3291dc37eb32234a9c229654db05a5dda7576
Riccardo Rimpici <133099369+rimpx@users.noreply.github.com>
2024-05-02 10:52:27 +0000

    modifica player video

--- src/main.js
+++ src/main.js
@@ -1,15 +1,27 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+// Video.js styles
+import 'video.js/dist/video-js.css'
+
+// Import VideoPlayer as a named export
+import { VideoPlayer } from 'vue-hls-video-player'
+
 const vuetify = createVuetify({
   components,
   directives,
 })
 
-createApp(App).use(router).use(vuetify).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(vuetify)
+app.use(VideoPlayer)  // Register the video player plugin
+
+app.mount('#app')
