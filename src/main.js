6bef38013b47ddb273ecdbde76cbd028207c79b7
Riccardo Rimpici <133099369+rimpx@users.noreply.github.com>
2024-05-03 15:36:36 +0000

    modifiche main e pacchetti icone

--- src/main.js
+++ src/main.js
@@ -2,33 +2,33 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-// Vuetify
+
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-// Importing Material Design Icons for Vuetify
-import '@mdi/font/css/materialdesignicons.css'  // Assicurati che questo percorso sia corretto
 
-// Video.js styles
+import '@mdi/font/css/materialdesignicons.css'  
+
+
 import 'video.js/dist/video-js.css'
 
-// Import VideoPlayer as a named export
+
 import { VideoPlayer } from 'vue-hls-video-player'
 
 const vuetify = createVuetify({
   components,
   directives,
   icons: {
-    iconfont: 'mdi',  // 'mdi' specifies to use Material Design Icons
+    iconfont: 'mdi',  
   }
 })
 
 const app = createApp(App)
 app.use(router)
 app.use(vuetify)
-app.use(VideoPlayer)  // Register the video player plugin
+app.use(VideoPlayer)  
 
 app.mount('#app')
 
