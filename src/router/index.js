f2f0575457868e5d6e34568bfcb3ae592796644a
Riccardo Rimpici <133099369+rimpx@users.noreply.github.com>
2024-05-04 13:50:40 +0000

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/FavouriteView.vue')
-  }
-]
+import FavoritesView from '../views/FavoritesView.vue'  // Assicurati di importare la vista dei preferiti
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: HomeView
+    },
+    {
+      path: '/favorites',
+      name: 'Favorites',
+      component: FavoritesView  // Aggiungi la route per i preferiti
+    }
+  ]
 })
 
 export default router
